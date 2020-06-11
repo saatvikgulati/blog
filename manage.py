f9bed64a8bae763408548d5a2d5261c05bceb0af
saatvikgulati <saatugulati@gmail.com>
2020-06-11 10:39:14 +0000

    blog

--- manage.py
+++ manage.py
@@ -3,19 +3,10 @@
 import os
 import sys
 
-
-def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
-    execute_from_command_line(sys.argv)
-
-
-if __name__ == '__main__':
-    main()
+path = os.path.expanduser('~/mysite')
+if path not in sys.path:
+    sys.path.insert(0, path)
+os.environ['DJANGO_SETTINGS_MODULE'] = 'myproject.settings'
+from django.core.wsgi import get_wsgi_application
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+application = StaticFilesHandler(get_wsgi_application())
